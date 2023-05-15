cb06c0fd104621569289e4136347f50cac8430b3
Jason-fos <>
2023-05-15 18:56:42 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,38 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b){
+//code block
+return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers(a, b) => {
+    //code block
+    return a + b;
+    }
+    
+    let sum = addTwoNumbers(3, 5);
+    console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello There!!');
+
+const sayHello = () => console.log('Hello!');
+sayHello();
 
 // Returning Multiple Lines
+const returningMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+    </p>`
+)
+
+console.log(returningMultipleLines);
